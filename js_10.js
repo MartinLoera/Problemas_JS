f5798ca1019a8fd4080f19ee0042e20fb1745041
martinLoera <a319094@uach.mx>
2021-04-27 21:25:24 -0600

    Problema 10 resuelto

--- js_10.js
+++ js_10.js
@@ -3,10 +3,10 @@ process.stdout.write("Dime un numero: ");
 
 process.stdout.on('data', function(data){
     num = parseInt(data)
-    if (num > 0) {
-        console.log(`El numero ${num} es positivo`)
-    }else if(num < 0) {
-        console.log(`El numero ${num} es negativo`)
+    if (num % 2 == 0) {
+        console.log(`El numero ${num} es par`)
+    }else if(num % 2 != 0) {
+        console.log(`El numero ${num} es impar`)
     } else {
         console.log(`Tu numero es 0`)
     }
